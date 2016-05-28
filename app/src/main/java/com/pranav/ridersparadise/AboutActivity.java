e8d22b8870126f5edfdf900c6d92348ac3693c34
Pranav <pranav_bhartia@yahoo.in>
2016-05-28 15:13:14 +0530

    added about and nearest petrol bunk menu items

---
+++ app/src/main/java/com/pranav/ridersparadise/AboutActivity.java
@@ -0,0 +1,37 @@
+package com.pranav.ridersparadise;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, MainActivity.class);
+                this.startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
--- app/src/main/java/com/pranav/ridersparadise/MainActivity.java
+++ app/src/main/java/com/pranav/ridersparadise/MainActivity.java
@@ -1,5 +1,8 @@
 package com.pranav.ridersparadise;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -8,9 +11,14 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +43,13 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main_actions, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         String[] brakeStrings = getResources().getStringArray(R.array.brakes);
@@ -42,11 +57,11 @@ public class MainActivity extends AppCompatActivity {
         String[] oilStrings = getResources().getStringArray(R.array.engine_oil);
         String[] tyreStrings = getResources().getStringArray(R.array.tyre_pressure);
         String[] sparkStrings = getResources().getStringArray(R.array.spark_plug);
-        adapter.addFragment(new MainFragment().newInstance(brakeStrings[0], brakeStrings[1], brakeStrings[2], brakeStrings[3]), "Brakes");
-        adapter.addFragment(new MainFragment().newInstance(oilStrings[0], oilStrings[1], oilStrings[2], oilStrings[3]), "Engine Oil");
-        adapter.addFragment(new MainFragment().newInstance(tyreStrings[0], tyreStrings[1], tyreStrings[2], tyreStrings[3]), "Tyre Pressure");
-        adapter.addFragment(new MainFragment().newInstance(chainStrings[0], chainStrings[1], chainStrings[2], chainStrings[3]), "Chain");
-        adapter.addFragment(new MainFragment().newInstance(sparkStrings[0], sparkStrings[1], sparkStrings[2], sparkStrings[3]), "Spark Plugs");
+        adapter.addFragment(MainFragment.newInstance(brakeStrings[0], brakeStrings[1], brakeStrings[2], brakeStrings[3]), "Brakes");
+        adapter.addFragment(MainFragment.newInstance(oilStrings[0], oilStrings[1], oilStrings[2], oilStrings[3]), "Engine Oil");
+        adapter.addFragment(MainFragment.newInstance(tyreStrings[0], tyreStrings[1], tyreStrings[2], tyreStrings[3]), "Tyre Pressure");
+        adapter.addFragment(MainFragment.newInstance(chainStrings[0], chainStrings[1], chainStrings[2], chainStrings[3]), "Chain");
+        adapter.addFragment(MainFragment.newInstance(sparkStrings[0], sparkStrings[1], sparkStrings[2], sparkStrings[3]), "Spark Plugs");
         viewPager.setAdapter(adapter);
     }
 
@@ -76,6 +91,39 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public CharSequence getPageTitle(int position) {
             return mFragmentTitleList.get(position);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Take appropriate action for each action item click
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                Intent intent = new Intent(this, AboutActivity.class);
+                this.startActivity(intent);
+                return true;
+            case R.id.action_nearest_petrol_bunks:
+                String uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?&daddr=nearby petrol bunk");
+                Intent mapsIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                mapsIntent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+                try
+                {
+                    startActivity(mapsIntent);
+                }
+                catch(ActivityNotFoundException ex)
+                {
+                    try
+                    {
+                        Intent unrestrictedIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                        startActivity(unrestrictedIntent);
+                    }
+                    catch(ActivityNotFoundException innerEx)
+                    {
+                        Toast.makeText(this, "Please install a maps application", Toast.LENGTH_LONG).show();
+                    }
+                }
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
